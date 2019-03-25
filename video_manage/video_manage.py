ed78ddf4e87a0a2258487de4e19ffe92028227d9
motya0414 <motya0414@hotmail.co.jp>
2019-03-26 01:38:33 +0900

    [change] ソースコードの整形

--- video_manage/video_manage.py
+++ video_manage/video_manage.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python
 # coding: utf-8
 
+import json
+import hashlib
+import os.path
+import subprocess
+import configparser
 '''
 動画管理を行うプログラム
 CONFIGファイルに書かれているディレクトリ内に存在する動画に関する情報をまとめて、JSON形式で出力する。
 '''
 
 
-import io,sys,codecs
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+import io
+import sys
+import codecs
 import configparser
 import subprocess
 import os.path
 import hashlib
 import json
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
 
 #############################################
 #                  classes                  #
 #############################################
+
+
 class VIDEO:
-    def __init__(self, video_dir_path, video_file_name, sha1=None, video_length=0, tags=[], exists_video_file=False,exists_thumbnail=False, view_count=0):
+    def __init__(self, video_dir_path, video_file_name, sha1=None, video_length=0, tags=[], exists_video_file=False, exists_thumbnail=False, view_count=0):
         self.video_dir_path = video_dir_path
         self.video_file_name = video_file_name
         self.sha1 = sha1
@@ -41,12 +51,13 @@ class VIDEO:
         -------
         ハッシュ値(str)
         '''
-        video_path=self.video_dir_path+"/"+self.video_file_name
+        video_path = self.video_dir_path+"/"+self.video_file_name
         with open(video_path, "rb") as video_file:
             video_binary_data = video_file.read()
             sha1 = hashlib.sha1(video_binary_data).hexdigest()
         if DEBUG in ["1", "2"]:
-            print("*INFO: {0}'s sha1sum->{1}".format(self.video_file_name, sha1))
+            print(
+                "*INFO: {0}'s sha1sum->{1}".format(self.video_file_name, sha1))
         self.sha1 = sha1
         return self.sha1
 
@@ -66,7 +77,7 @@ class VIDEO:
         if os.path.isfile(self.video_dir_path + "/" + tmb_file_name + ".jpg"):
             if DEBUG in ["2"]:
                 print("*INFO: thumbnail not exists")
-            self.exists_thumbnail=True
+            self.exists_thumbnail = True
         else:
             if DEBUG in ["2"]:
                 print("*INFO: thumbnail exists")
@@ -90,7 +101,8 @@ class VIDEO:
         import cv2
 
         video_path = self.video_dir_path + "/" + self.video_file_name
-        thumbnail_path = self.video_dir_path + "/" + self.video_file_name[:self.video_file_name.rfind(".")] + ".jpg"
+        thumbnail_path = self.video_dir_path + "/" + \
+            self.video_file_name[:self.video_file_name.rfind(".")] + ".jpg"
         video = cv2.VideoCapture(video_path)
         video_frame = video.get(cv2.CAP_PROP_FRAME_COUNT)
 
@@ -113,7 +125,7 @@ class VIDEO:
                     temp_image_list = []
                     hor_img_count = 0
 
-            flame_num+=1
+            flame_num += 1
 
         # メモリ開放
         video.release()
@@ -143,14 +155,14 @@ class VIDEO:
         import numpy as np
         import cv2
 
-        video_path=self.video_dir_path+"/"+self.video_file_name
+        video_path = self.video_dir_path+"/"+self.video_file_name
         video = cv2.VideoCapture(video_path)
 
         video_frame = video.get(cv2.CAP_PROP_FRAME_COUNT)
         video_fps = video.get(cv2.CAP_PROP_FPS)
         video_len_sec = video_frame / video_fps
 
-        self.video_length=video_len_sec
+        self.video_length = video_len_sec
 
         return video_len_sec
 
@@ -202,11 +214,12 @@ def search_video_file(search_path):
         内容: 動画ファイルに関する情報が入ったリスト
     '''
     # initialize
-    video_file_list=[]
+    video_file_list = []
 
     if DEBUG in ["1", "2"]:
         print("*INFO: in dir:{0}".format(search_path))
-    proc = subprocess.run(["ls", "-1"], cwd=search_path, stdout=subprocess.PIPE)
+    proc = subprocess.run(["ls", "-1"], cwd=search_path,
+                          stdout=subprocess.PIPE)
     ls_results = proc.stdout.decode("utf8").split()
 
     for ls_result in ls_results:
@@ -221,6 +234,7 @@ def search_video_file(search_path):
         print("*INFO: out dir: {0}".format(search_path))
 
     return video_file_list
+
 
 def get_video_data(video_dir_path, video_file_name):
     '''
@@ -244,11 +258,13 @@ def get_video_data(video_dir_path, video_file_name):
     if DEBUG in ["1", "2"]:
         print("*INFO: get {0} data".format(video_file_name))
 
-    video = VIDEO(video_dir_path=video_dir_path, video_file_name=video_file_name, exists_video_file=True)
+    video = VIDEO(video_dir_path=video_dir_path,
+                  video_file_name=video_file_name, exists_video_file=True)
     video.calc_hash()
     video.check_exists_thumbnail()
 
     return video
+
 
 def exists_video_data(json_video_data_list, sha1):
     '''
@@ -278,6 +294,7 @@ def exists_video_data(json_video_data_list, sha1):
 
     return exists_video_data
 
+
 def set_video_data_for_json(json_video_data_dict):
     '''
     JSONのデータをVIDEOクラスのデータに変換する
@@ -294,9 +311,9 @@ def set_video_data_for_json(json_video_data_dict):
         type:VIDEOクラス
         内容:動画のデータ
     '''
-    video_data = VIDEO(video_dir_path = json_video_data_dict["video_dir_path"],
-    video_file_name = json_video_data_dict["video_file_name"],
-    sha1 = json_video_data_dict["sha1"])
+    video_data = VIDEO(video_dir_path=json_video_data_dict["video_dir_path"],
+                       video_file_name=json_video_data_dict["video_file_name"],
+                       sha1=json_video_data_dict["sha1"])
 
     if "video_length" in json_video_data_dict:
         video_data.video_length = json_video_data_dict["video_length"]
@@ -307,9 +324,10 @@ def set_video_data_for_json(json_video_data_dict):
     if "exists_thumbnail" in json_video_data_dict:
         video_data.exists_thumbnail = json_video_data_dict["exists_thumbnail"]
     if "view_count" in json_video_data_dict:
-        video_data.view_count=json_video_data_dict["view_count"]
+        video_data.view_count = json_video_data_dict["view_count"]
 
     return video_data
+
 
 def check_json_format(json_file_path):
     '''
@@ -332,6 +350,7 @@ def check_json_format(json_file_path):
             return True
         except json.JSONDecodeError:
             return False
+
 
 def update_video_data(json_video_data_list, dir_video_data):
     '''
@@ -369,28 +388,30 @@ def update_video_data(json_video_data_list, dir_video_data):
 #                   main                    #
 #############################################
 
+
 # initialize
 config_file = configparser.ConfigParser()
 config_file.read('video_manage.conf', 'UTF-8')
 ROOT_VIDEO_DIR_LIST = config_file.get("SETTINGS", "root_video_dir").split(",")
-ROOT_WEB_DIR=config_file.get("SETTINGS", "root_web_dir")
+ROOT_WEB_DIR = config_file.get("SETTINGS", "root_web_dir")
 DEBUG = config_file.get("DEBUG", "DEBUG_LEVEL")
 
 
 # ディレクトリ内の動画ファイルをリスト化
-dir_video_data_list=[]
+dir_video_data_list = []
 for ROOT_VIDEO_DIR in ROOT_VIDEO_DIR_LIST:
     dir_video_data_list.extend(search_video_file(ROOT_VIDEO_DIR))
 
 
 # videos.json内の動画データをリスト化
-json_video_data_list=[]
+json_video_data_list = []
 if os.path.isfile(ROOT_WEB_DIR + "/videos.json") == True and check_json_format(ROOT_WEB_DIR + "/videos.json") == True:
     with open(ROOT_WEB_DIR + "/videos.json", "r") as video_json_file:
         json_video_data = json.load(video_json_file)
         # json_video_data(辞書型)のデータをVIDEOクラスのリスト(json_video_data_list)に変換
         for json_video_data_dict in json_video_data:
-            json_video_data_list.append(set_video_data_for_json(json_video_data_dict))
+            json_video_data_list.append(
+                set_video_data_for_json(json_video_data_dict))
 
 
 # dir_video_data_listとjson_video_data_listを比較
@@ -399,25 +420,26 @@ for dir_video_data in dir_video_data_list:
         # 新規追加
         dir_video_data.calc_video_length()
         json_video_data_list.append(dir_video_data)
-        if dir_video_data.check_exists_thumbnail()==FAlse:
+        if dir_video_data.check_exists_thumbnail() == False:
             dir_video_data.make_thumbnail()
     elif exists_video_data(json_video_data_list, dir_video_data.sha1) == True:
         # ファイルパスなどをアップデート
-        json_video_data_list = update_video_data(json_video_data_list, dir_video_data)
+        json_video_data_list = update_video_data(
+            json_video_data_list, dir_video_data)
     else:
         # 新規追加
         dir_video_data.calc_video_length()
         json_video_data_list.append(dir_video_data)
-        if dir_video_data.check_exists_thumbnail()==FAlse:
+        if dir_video_data.check_exists_thumbnail() == False:
             dir_video_data.make_thumbnail()
 
 
 # 書き出すデータの準備
-output_video_data_list=[]
+output_video_data_list = []
 for json_video_data in json_video_data_list:
     output_video_data_list.append(json_video_data.generate_dict())
 
 
 # video.jsonに書き出し
 with open(ROOT_WEB_DIR + "/videos.json", "w") as video_json_file:
-    json.dump(output_video_data_list,video_json_file,indent=4)
+    json.dump(output_video_data_list, video_json_file, indent=4)
