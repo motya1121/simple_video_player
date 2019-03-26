2bdef75486116baceb91a9e6c6dd730d73d01978
motya0414 <motya0414@hotmail.co.jp>
2019-03-26 15:43:05 +0900

    [add]動画のハッシュ値を受け取って、その動画を表示する処理を追加

--- web_page/watch.php
+++ web_page/watch.php
@@ -1,50 +1,59 @@
 <!DOCTYPE html>
 <html>
-    <head>
-	<meta charset="utf-8" />
-	<title>home video page</title>
-	<link rel="stylesheet" type="text/css" href="style_pc_debug.css">
-	</head>
-	<script>
-		var getParameterByName = function(name) {
-    		var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-    		return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-		}
-	</script>
 
-    <body bgcolor="#FAFAFA">
-		<script type="text/javascript" src="videos.js"></script>
-		<div class="header">
-			<div class="header_rogo">
-				<h1><a href="top.html" class="noline">home video page</a></h1>
-			</div>
-			<div class="header_account">
-			</div>
-		</div>
-		<div class="contents">
-			<div class="contents_panel">
-				<div class="contents_panel_search">
-					Search
-					<form action="top.html" method="get">
-						<input type="text" name="search" style="width:90%; box-sizing:border-box" maxlength="100" placeholder="検索文字列">	<br><input type="submit" value="検索">
-					</form>
-				</div>
-				<div class="contents_panel_recommend">
-				</div>
-			</div>
-			<div class="contents_main" id="contents_main">
-			</div>
-		</div>
-		<div class="bottom">
-			<a href="top.html">Home video page</a>
-		</div>
-	</body>
-	<script type="text/javascript">
-		window.onload=function () {
-			videos=getJSON("videos.json");
-			id=getParameterByName("id")
-			//write
-			write_video(videos,id)
-		};
-	</script>
-</html>
+<head>
+    <meta charset="utf-8" />
+    <title>home video page</title>
+    <link rel="stylesheet" type="text/css" href="style_pc.css">
+    <?php
+    ini_set('display_errors', "On");
+    # check exist GET data
+    if (empty($_GET["sha1"])) {
+        header('Location: top.php');
+        exit;
+    }
+
+    $json_file = "videos.json";
+    if (file_exists($json_file)) {
+        $json = file_get_contents($json_file);
+        $video_datas = json_decode($json, true);
+    } else {
+        echo "データがありません";
+    }
+    ?>
+</head>
+
+<body bgcolor="#FAFAFA">
+    <div class="header">
+        <div class="header_rogo">
+            <h1><a href="top.php" class="noline">home video page</a></h1>
+        </div>
+    </div>
+    <div class="contents">
+        <div class="contents_panel">
+            <div class="contents_panel_search">
+                Search
+                <form action="top.html" method="get">
+                    <input type="text" name="search" style="width:90%; box-sizing:border-box" maxlength="100" placeholder="検索文字列"> <br><input type="submit" value="検索">
+                </form>
+            </div>
+        </div>
+        <div class="contents_main">
+            <?php
+            foreach ($video_datas as $video_data) {
+                if ($video_data["sha1"] == $_GET["sha1"]) {
+                    $video_dir_path = "video_contents/" . sha1($video_data["video_dir_path"]);
+                    echo '<H2>' . substr($video_data["video_file_name"], 0, strrpos($video_data["video_file_name"], ".")) . '</H2>';
+                    echo '<video src="' . $video_dir_path . '/' . $video_data["video_file_name"] . '" controls></video>';
+                }
+            }
+            ?>
+        </div>
+    </div>
+    <div class="bottom">
+        <a href="top.php">Home video page!</a>
+    </div>
+</body>
+
+
+</html> 
