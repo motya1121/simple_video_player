a8c06235f0651695dcfef40039a5cb70cdc695ec
motya0414 <motya0414@hotmail.co.jp>
2019-03-26 15:52:02 +0900

    [add] 再生回数を増やす処理を追加

--- web_page/watch.php
+++ web_page/watch.php
@@ -40,20 +40,27 @@
         </div>
         <div class="contents_main">
             <?php
+            $index_count = 0;
             foreach ($video_datas as $video_data) {
                 if ($video_data["sha1"] == $_GET["sha1"]) {
                     $video_dir_path = "video_contents/" . sha1($video_data["video_dir_path"]);
                     echo '<H2>' . substr($video_data["video_file_name"], 0, strrpos($video_data["video_file_name"], ".")) . '</H2>';
                     echo '<video src="' . $video_dir_path . '/' . $video_data["video_file_name"] . '" controls></video>';
+                    $video_datas[$index_count]["view_count"] += 1;
                 }
+                $index_count += 1;
             }
+
+            $json = fopen($json_file, 'w');
+            fwrite($json, json_encode($video_datas));
+            fclose($json);
             ?>
         </div>
     </div>
     <div class="bottom">
         <a href="top.php">Home video page!</a>
     </div>
-</body>
+    </bo dy>
 
 
 </html> 
